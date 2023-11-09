109d13f95092a77e119522545839d5fc3fc72346
Bishar Abdinur Mohamed <134773924+bisharr@users.noreply.github.com>
2023-11-09 07:01:48 +0300

    Add files via upload

--- end.js
+++ end.js
@@ -5,6 +5,10 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+const max_high_scores = 5;
+
 finalScore.innerHTML = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -15,4 +19,18 @@ username.addEventListener('keyup', () => {
 saveScoreBtn.addEventListener('click', function (e) {
   console.log('saveScoreBtn clicked');
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value,
+  };
+  highScores.push(score);
+
+  highScores.sort((a, b) => b.score - a.score);
+
+  highScores.splice(5);
+
+  localStorage.getItem('highScores', JSON.stringify(highScores));
+
+  window.location.assign('/');
 });
